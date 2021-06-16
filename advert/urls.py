7a132fe53196d4cf9c298f140f1ed9680011abba
Marleono <73365729+Marleono@users.noreply.github.com>
2021-06-16 21:42:10 +0300

    Update urls.py

--- advert/urls.py
+++ advert/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AnnouncementsList, AnnouncementDetail, AnnouncementCreate, AnnouncementDelete, ResponseCreate, ResponseList
+from .views import AnnouncementsList, AnnouncementDetail, AnnouncementCreate, AnnouncementUpdate, AnnouncementDelete, ResponseCreate, ResponseList
 
 
 urlpatterns = [
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', AnnouncementsList.as_view()), # т.к. сам по себе это класс, то нам надо представить этот класс в виде view. Для этого вызываем метод as_view
     path('<int:pk>', AnnouncementDetail.as_view(), name='announcement'),
     path('create/', AnnouncementCreate.as_view(), name='announcement_create'),
+    path('<int:pk>/update/', AnnouncementUpdate.as_view(), name='announcement_create'),
     path('<int:pk>/delete/', AnnouncementDelete.as_view(), name='announcement_delete'),
     path('<int:pk>/response/', ResponseCreate.as_view(), name='response_create'),
     path('profile/', ResponseList.as_view(), name='profile'),
